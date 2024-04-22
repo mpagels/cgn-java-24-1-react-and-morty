90b7a1dfaa9e77522a0f882fc5f80ee8fb64b2bd
Martin Pagels <office@martinpagels.de>
2024-04-22 10:04:18 +0200

    feat: app can fetch rick and morty characters and render the stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import "./App.css"
+import axios from "axios";
+import {useEffect, useState} from "react";
 function App() {
 
+    const [characters, setCharacters] = useState([])
+
+    function getCharacters() {
+        axios.get("https://rickandmortyapi.com/api/character").then(response => setCharacters(response.data.results))
+    }
+
+    useEffect(() => {getCharacters()}, [])
+
   return (
+
     <>
-     Hello World
+        <h1>React and Morty</h1>
+        <ul>
+        {characters.map(character => {
+            return <li key={character.id}>
+                <h2>{character.name}</h2>
+                <img src={character.image} alt={""} />
+                <h3>Gender: {character.gender}</h3>
+                <h4>Status: {character.status}</h4>
+            </li>
+        })}
+        </ul>
     </>
   )
 }
